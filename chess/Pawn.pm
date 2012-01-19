16d8a493697c0da42b6d80be0b472b79e27cf8ff
tommyxd <tomislav.dyulgerov@gmail.com>
2012-01-19 13:53:17 +0200

    Update chess/Pawn.pm

--- chess/Pawn.pm
+++ chess/Pawn.pm
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -w
 # A class representing the pawn chess piece.
+# A pawn is the only chess piece that can only move forward.
 # A pawn can move one square at a time, or two squares on its first move.
-# A pawn captures pieces one square diagonally(only forward).
+# A pawn captures pieces one square diagonally.
 # A pawn can promote itself to a Knight(N), Bishop(B), Rook(R) or Queen(Q).
 package Pawn;
 use strict;
