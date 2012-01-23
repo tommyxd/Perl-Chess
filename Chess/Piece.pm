b0ab3ed6d84b12548abc034504a8430d1f92e048
Tomislav Dyulgerov <tomislav.dyulgerov@gmail.com>
2012-01-23 23:16:54 +0200

    adding Piece.pm

---
+++ Chess/Piece.pm
@@ -0,0 +1,39 @@
+=head1 NAME
+
+Chess::Piece - The base class of all chess pieces.
+
+=head1 DESCRIPTION
+
+
+=head1 METHODS
+
+=head2 Construction
+
+=item new()
+
+Creates a new instance of the Chess::Piece class.
+
+=head2 Class methods
+
+There are no class methods for this class.
+
+=head2 Object methods
+
+=item
+
+=head1 AUTHOR
+
+Tomislav Dyulgerov
+
+=cut
+package Chess::Piece;
+use strict;
+
+# Constructor method stub.
+sub new {
+    my $class = shift;
+    my $self  = {};
+    # TODO: Not implemented yet.
+    bless $self, $class;
+    return $self;
+}
