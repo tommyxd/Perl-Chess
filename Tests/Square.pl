80155321a192d2c638a1997a6535f56a8d22ca1c
Tomislav Dyulgerov <tomislav.dyulgerov@gmail.com>
2012-01-29 20:19:33 +0200

    Modifying some files.

--- Tests/Square.pl
+++ Tests/Square.pl
@@ -3,12 +3,32 @@
 # Testing the Square.pm module.
 
 use strict;
-use Test::More tests => 2;
+use lib "E:\\Sources\\Perl\\Perl-Chess\\";
+use Test::More tests => 7;
 use Chess::Square;
 
 my $square = Chess::Square->new();
+
 $square->file('A');
 my $file = $square->file;
 
-ok $file eq 'A', "Square's file is equal to the expected value.";
-ok $file ne 'C', "Square's file is not equal to the incorrect value.";
+ok $file eq 'a', "Square's file is equal to the expected value.";
+ok $file ne 'C', "Square's file is not equal to an incorrect value.";
+ok $file ne 'A', "Square's file is converted to lowercase.";
+
+$square->rank(3);
+my $rank = $square->rank;
+
+ok $rank eq 3, "Square's rank is equal the expected value";
+ok $rank ne 4, "Square's rank is not equal to an incorrect value";
+
+$square->color('Light');
+my $light_square = $square->color;
+
+my $second_square = Chess::Square->new();
+
+$second_square->color('dARk');
+my $dark_square = $second_square->color;
+
+ok $light_square eq 'light', "This is a light squre";
+ok $dark_square  eq 'dark',  "This is a dark square";
